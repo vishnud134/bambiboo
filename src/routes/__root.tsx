dd6fd03bcd7d13bf3b872f6628f0b07c7b2ef111
Vishnu D <vishnudevakaran@gmail.com>
2026-08-18 15:56:01 +0530

    Update website favicon to new BambiBoo hatching chick icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -103,7 +103,9 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "shortcut icon", href: "/favicon.ico" },
+      { rel: "apple-touch-icon", href: "/favicon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
